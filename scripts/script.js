d6a1138fd77a72e122dca76d1fc0a2a821785837
Татьяна Гурьянова <t.a.gurianova@yandex.ru>
2022-06-23 20:06:36 +0300

    style and functional changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,24 @@
-let inputList = document.querySelector('.input-container');
-let sortButton = document.querySelector('.sort-tasks');
-let ifSortAlfa = -1;
-
-//Создаю элемент крестик и сразу навешиваю на него обработчик события
-document.querySelector('.delete').addEventListener('click', () => { 
-    if (inputList.children.length > 1) {
-        document.querySelector('.delete').parentElement.remove();
-    } else {
-        document.querySelector('.delete').parentElement.querySelector('input').value = '';
-    } 
-});
-
+const inputList = document.querySelector('.input-container');
+const sortButton = document.querySelector('.sort-tasks');
+let sortABC = -1;
 
 document.querySelector('.action').addEventListener('click', () => {
     let newTask = document.createElement('div');
     newTask.classList.add('item');
-    newTask.innerHTML = `<input class="task" type="text">
-    <button class="delete">X</button>`;
+    newTask.innerHTML = `
+        <input class="task" type="text">
+        <button class="delete">X</button>
+        `;
     inputList.append(newTask);
-   
+
     newTask.querySelector('.delete').addEventListener('click', () => {
         if (inputList.children.length > 1) {
-            newTask.remove();
+            document.querySelector('.delete').parentElement.remove();
         } else {
             newTask.querySelector('input').value = '';
-        } 
+        }
     });
-});    
+});
 
 
 sortButton.addEventListener('click', () => {
@@ -34,14 +26,14 @@ sortButton.addEventListener('click', () => {
     document.querySelectorAll('.task').forEach((item) => {
         sortRow.push(item.value);
     });
-    ifSortAlfa = !sortButton.classList.contains('reversed') ? 1 : -1;
-    sortRow.sort((a, b) => (a > b ? ifSortAlfa : ifSortAlfa * -1));
+    sortABC = !sortButton.classList.contains('reversed') ? 1 : -1;
+    sortRow.sort((a, b) => (a > b ? sortABC : sortABC * -1));
     
     document.querySelectorAll('.task').forEach((item, index) => {
         item.value = sortRow[index];
     });
     
     sortButton.classList.toggle('reversed');
-})
+});
 
 
